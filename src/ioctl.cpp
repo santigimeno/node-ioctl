7443f7708cb646294e9eb5cf5c59d080e3d89eba
Santiago Gimeno <santiago.gimeno@ionide.es>
2015-09-11 13:46:11 +0200

    deps: upgrade to nan@2 to support node 4

--- src/ioctl.cpp
+++ src/ioctl.cpp
@@ -6,48 +6,49 @@ using namespace v8;
 using namespace node;
 
 NAN_METHOD(Ioctl) {
-    NanScope();
+    Nan::HandleScope scope;
+
     Local<Object> buf;
-    int length = args.Length();
+    int length = info.Length();
 
     assert(length == 3);
 
     void* argp = NULL;
 
-    if (!args[0]->IsInt32()) {
-		NanThrowTypeError("Argument 0 Must be an Integer");
+    if (!info[0]->IsInt32()) {
+        Nan::ThrowTypeError("Argument 0 Must be an Integer");
     }
 
-    if (!args[1]->IsUint32()) {
-		NanThrowTypeError("Argument 1 Must be an Integer");
+    if (!info[1]->IsUint32()) {
+        Nan::ThrowTypeError("Argument 1 Must be an Integer");
     }
 
-    if (!args[2]->IsUndefined()) {
-        if (args[2]->IsInt32()) {
-            argp = reinterpret_cast<void*>(args[2]->Int32Value());
+    if (!info[2]->IsUndefined()) {
+        if (info[2]->IsInt32()) {
+            argp = reinterpret_cast<void*>(info[2]->Int32Value());
         } else {
-            buf = args[2]->ToObject();
+            buf = info[2]->ToObject();
             if (!Buffer::HasInstance(buf)) {
-                NanThrowTypeError("Argument 2 Must be an Integer or a Buffer");
+                Nan::ThrowTypeError("Argument 2 Must be an Integer or a Buffer");
             }
 
             argp = Buffer::Data(buf);
         }
     }
 
-    int fd = args[0]->Int32Value();
-    unsigned long request = args[1]->IntegerValue();
+    int fd = info[0]->Int32Value();
+    unsigned long request = info[1]->IntegerValue();
     int res = ioctl(fd, request, argp);
     if (res == -1) {
         res = -errno;
     }
 
-    NanReturnValue(NanNew(res));
+    info.GetReturnValue().Set(res);
 }
 
-void InitAll(Handle<Object> exports) {
-    exports->Set(NanNew("ioctl"),
-                 NanNew<FunctionTemplate>(Ioctl)->GetFunction());
+void InitAll(Local<Object> exports) {
+    exports->Set(Nan::New("ioctl").ToLocalChecked(),
+                 Nan::New<FunctionTemplate>(Ioctl)->GetFunction());
 }
 
 NODE_MODULE(ioctl, InitAll)
